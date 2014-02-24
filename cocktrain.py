24c02f0a23caeb3c950d4df11464f59f51891655
lwills <piratelaurrra@gmail.com>
2014-02-24 01:30:00 -0500

    added in predictions on test data

--- cocktrain.py
+++ cocktrain.py
@@ -52,7 +52,43 @@ def readCSV(filename):
 	print "%d rows in %s" % (len(rows), filename)
 	return rows
 
+
+'''
+takes in the training features and the training labels
+prints out the performance accuracy
+'''
+def testOnTraining(feats,labels):
+	split_row = int(feats.shape[0]*.8)
+
+	feats_train = feats[:split_row,:]
+	feats_test = feats[split_row:,:]
+	label_train = labels[:split_row]
+	label_test = labels[split_row:]
+
+	clf = svm.SVC(kernel="rbf")
+	clf.fit(feats_train,label_train)
+	print "%d training rows, %d features" % (feats_train.shape)
+	print "%d testing rows" % label_test.shape
+	results = clf.score(feats_test,label_test)
+	print "%f performance accuracy" % results
+
+
+'''
+write out a file with the predicted labels of the test set
+'''
+def createPredictions(feats_train,label_train,feats_test):
+	clf = svm.SVC(kernel="rbf")
+	clf.fit(feats_train,label_train)
+	predictions = clf.predict(feats_test)
+	filename = "cocktanic_submission.csv"
+	with open(filename,"wb") as csvfile:
+		writer = csv.writer(csvfile)
+		for p in predictions:
+			writer.writerow(p)
+		
+
 def main():
+	# get a predicted accuracy
 	train_filename = "train.csv"
 	train_rows = readCSV(train_filename)
 
@@ -60,21 +96,24 @@ def main():
 	header = list(data[0,:])
 	data = data[1:,:]
 
-	split_row = 700
-	label_train = data[:split_row,1]
-	label_test = data[split_row:,1]
+	label_train = data[:,1]
 
 	feats = getFeatures(header,data)
-	feats_train = feats[:split_row,:]
-	feats_test = feats[split_row:,:]
+	testOnTraining(feats,label_train)
 
-	clf = svm.SVC(kernel="rbf")
-	clf.fit(feats_train,label_train)
-	print feats_test.shape
-	print label_test.shape
-	results = clf.score(feats_test,label_test)
-	print results
+	# generate predictions on test data
+	test_filename = "test.csv"
+	test_rows = readCSV(test_filename)
+
+	data_test = np.array(test_rows)
+	header_test = list(data_test[0,:])
+	data_test = data_test[1:,:]
+
+	label_test = data_test[:,1]
+	feats_test = getFeatures(header_test,data_test)
 	
+	createPredictions(feats,label_train,feats_test)
+
 
 if __name__=='__main__':
 	main()
